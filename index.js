809ba43306dcfb5351d92111bb84833424cc5dee
Sharon Walters <SHARONMARIE919@GMAIL.COM>
2020-08-02 19:57:00 -0400

    tried on my own

--- index.js
+++ index.js
@@ -10,10 +10,13 @@ selector and the style property, write pure JavaScript to reveal
 who's behind the remaining boxes. 
 ________________________________________________________________*/
 
-// Your code here
+let boxTw0 = document.querySelector("#box-two")
+console.log(boxTwo);
+// boxTwo.style.opacity = 1;
 
+// let boxNine = document.querySelector("#box-nine")
 
-
+// boxNine.style.opacity = 1;
 
 
 /*________________________________________________________________
@@ -23,9 +26,3 @@ Lastly, we've added an ID of "hidden-message" to an empty paragrah
 element. Use JavaScript to inject the paragragh's inner HTML with a 
 string containing whatever text you want.
 ________________________________________________________________*/
-
-// Your code here 
-
-
-
-
